11bff728caa1893eddd9ef2803eb9a4185372707
islamosman2541998 <islamosman2541998@gmail.com>
2026-06-03 21:56:35 +0300

    Update Visa.php

--- app/Http/Livewire/Site/Payments/Visa.php
+++ app/Http/Livewire/Site/Payments/Visa.php
@@ -9,20 +9,20 @@ use Livewire\Component;
 class Visa extends Component
 {
     public $payment_method_id = 2, $payment_method_key = 'Visa';
-    public $card_number = "", $card_name = "", $expired_year = "", $expired_month = "", $cvv = "";
+    public $card_number ="", $card_name ="", $expired_year ="", $expired_month ="", $cvv ="";
     public $selected_card = "", $selected_cvv = "", $savecard = "";
     public $myCards = [], $showNewCard = true;
 
     protected $listeners = ['updateAuth'];
 
-    protected function rules()
-    {
-        if ($this->selected_card) {
+    protected function rules(){
+        if($this->selected_card){
             return [
                 'selected_card' => 'required',
                 'selected_cvv'  => 'required|min:3|max:3',
             ];
-        } else {
+        }
+        else{
             return [
                 'card_number'   => 'required|min:16|max:16',
                 'card_name'     => 'required|min:3',
@@ -32,6 +32,7 @@ class Visa extends Component
                 'savecard'      => 'nullable',
             ];
         }
+
     }
 
     // public function updated($field)
@@ -47,11 +48,10 @@ class Visa extends Component
         return $data;
     }
 
-    public function checkout()
-    {
+    public function checkout(){
         // checkinig if missing data
         $missId = $this->checkMissingData();
-        if ($missId) {
+        if($missId){
             $this->emit('openModal', $missId);
             return false;
         }
@@ -60,59 +60,45 @@ class Visa extends Component
         return redirect(route('site.payments.intital', $data));
     }
 
-    public function checkMissingData()
-    {
+    public function checkMissingData(){
         $cart = new DatabaseCart();
         $cartItems = $cart->getItemsWithInfo();
         $products = $cartItems['cart']->where('item_type', 'App\Models\Product')->where('givten_details', null);
-        if (!empty($products->first())) {
+        if(!empty($products->first())){
             return $products->first()->id;
         }
     }
 
-    public function UpdatedSelectedCard()
-    {
+    public function UpdatedSelectedCard(){
         $this->showNewCard = 0;
         $this->selected_cvv = "";
     }
-    public function addNewCardBlock()
-    {
+    public function addNewCardBlock(){
         $this->showNewCard = $this->showNewCard ?  0 : 1;
         $this->selected_card = "";
     }
 
     public function updateAuth()
     {
-        if (@auth('account')->user()?->types->where('type', 'donor')->first() != null) {
+        if(@auth('account')->user()?->types->where('type', 'donor')->first() != null){
             $this->showNewCard = false;
             $this->render();
             $this->mount();
         }
     }
 
-    public function mount()
-    {
-        $this->myCards = collect();
-        $this->showNewCard = true;
-
-        $account = auth('account')->user();
-
-        if ($account) {
-            $donor = $account->donor;
-
-            if ($donor) {
-                $this->myCards = $donor->cards ?? collect();
-                $this->showNewCard = false;
-            }
+    public function mount(){
+        if(@auth('account')->user()?->types->where('type', 'donor')->first() != null){
+           $donor = @auth('account')->user()->donor;
+           $this->myCards = $donor->cards;
+           $this->showNewCard = false;
         }
-
-        $paymentMethod = PaymentMethod::find($this->payment_method_id);
-
-        $this->payment_method_key = $paymentMethod?->payment_key;
+        $this->payment_method_key = @PaymentMethod::find($this->payment_method_id)->payment_key;
     }
 
     public function render()
     {
         return view('livewire.site.payments.visa');
     }
+
 }
